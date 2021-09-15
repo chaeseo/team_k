545268cb42038527d0676b81722737f16a775d26
akshwpsh <84263687+akshwpsh@users.noreply.github.com>
2021-09-15 13:36:30 +0900

    현민_테스트

--- PlayCar/src/Screen.java
+++ PlayCar/src/Screen.java
@@ -29,6 +29,7 @@ public class Screen extends Canvas implements KeyListener {
 		// TODO Auto-generated method stub
 		super.paint(g);
 		g.drawImage(image, x, y, this);
+		g.drawImage(image, x+50, y, this);
 	}
 	@Override
 	public void keyTyped(KeyEvent e) {
